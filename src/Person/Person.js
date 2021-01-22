b0e6a408fb50f709481be0c654b74ed390b0a3ae
America Rivas <amerivasmur@gmail.com>
2021-01-22 16:59:11 -0400

    Using the children Prop

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,19 @@
 // Creating my first component
 
+//prop.children includes everything between the opening and closing tag of our component.
+// We can pass an html structure and not only external arguments.
+
 import React from 'react';
 
 const person = (props) => {
-	return <p> My name is {props.name} and I'm {props.age}! </p>
+	return (
+		<div>
+			<p> My name is {props.name} and I'm {props.age}! </p>
+			<p> {props.children} </p> 
+			
+		</div>
+	)
+	
 }
 
 export default person;
