39d806c754924d611b144b8dbff2e6c513439d9c
America Rivas <amerivasmur@gmail.com>
2021-01-22 18:45:03 -0400

    Using the useState() for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component{
-
-//state belongs to the class-based component
-  state = {
-    persons:[
-      { name: 'Max', age:"50"},
-      { name: 'Manu', age:"51"},
-      { name: 'Stephanie', age:"52"},
-    ],
-    otherState: 'some other value'
-  }
-
-
-switchNameHandler = () => {
-//console.log('Click');
-// DON'T DO THIS:this.setstate.persons[0].name = 'Maximilian';
-//The “setState()” is used to update this special state property. It will merge whatever it is defined there with the existing state.
-//What React do is look at the first state and see which part of it was overwritten or changed.
-    this.setState({
-      persons:[
-      { name: 'Maximilian', age:"100"},
-      { name: 'Manu', age:"51"},
-      { name: 'Stephanie', age:"52"},
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ]
-  })
-}
+  });
 
+  const [otherState, setOtherState] = useState('some other value');
 
-//Props are passed from outside
-//<!-- {}: dynamic content. -->
-  render(){ //Method render
-    return (
-        <div className="App">
-          <h1> Hello World </h1>
-          <p> This is really working </p>
-          <button onClick={this.switchNameHandler}> Switch Name! </button> 
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My hobby is: swimming </Person>
-        </div>
-      );
-  }
-}
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+};
 
 export default App;
