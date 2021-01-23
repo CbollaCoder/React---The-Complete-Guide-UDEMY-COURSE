b4b763a21ef23de1334e3cb98528ffd28eebd50d
America Rivas <amerivasmur@gmail.com>
2021-01-22 21:11:57 -0400

    Passing Method references Between Components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,14 @@
 
 //prop.children includes everything between the opening and closing tag of our component.
 // We can pass an html structure and not only external arguments.
+//
 
 import React from 'react';
 
 const person = (props) => {
 	return (
 		<div>
-			<p> My name is {props.name} and I'm {props.age}! </p>
+			<p onClick={props.click}> My name is {props.name} and I'm {props.age}! </p>
 			<p> {props.children} </p> 
 		</div>
 	)
